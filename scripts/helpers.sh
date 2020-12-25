32f9592d95c827fba4fc2904d0f9f19aacd8baca
Nelson Estevão <nelsonmestevao@gmail.com>
2020-12-25 23:36:08 +0000

    Update helper scripts

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -1,91 +1,83 @@
 #!/usr/bin/env bash
 
-function colorize() {
-  local color type=0
+VERSION=0.2.2
 
-  case $1 in
-    black)
-      color=30
-      ;;
-    red)
-      color=31
-      ;;
-    green)
-      color=32
-      ;;
-    yellow)
-      color=33
-      ;;
-    blue)
-      color=34
-      ;;
-    magenta)
-      color=35
-      ;;
-    cyan)
-      color=36
-      ;;
-    white)
-      color=37
-      ;;
-    reset | *)
-      color=0
-      ;;
-  esac
+tput sgr0
+RED=$(tput setaf 1)
+ORANGE=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+PURPLE=$(tput setaf 5)
+CYAN=$(tput setaf 4)
+BLUE=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
 
-  case $2 in
-    bold | bright)
-      type=1
-      ;;
-    underline)
-      type=4
-      ;;
-    inverse)
-      type=7
-      ;;
-  esac
-
-  echo -en "\\033[${type};${color}m"
+function log() {
+  local LABEL="$1"
+  local COLOR="$2"
+  shift 2
+  local MSG=("$@")
+  printf "[${COLOR}${BOLD}${LABEL}${RESET}]%*s" $(($(tput cols) - ${#LABEL} - 2)) | tr ' ' '='
+  for M in "${MSG[@]}"; do
+    let COL=$(tput cols)-2-${#M}
+    printf "%s%${COL}s${RESET}" "* $M"
+  done
+  printf "%*s\n" $(tput cols) | tr ' ' '='
 }
 
-function echo_error() {
-  colorize red bold
-  echo "ERROR:$(colorize reset)" "$@"
+function log_error() {
+  log "FAIL" "$RED" "$@"
 }
 
-function echo_warning() {
-  colorize yellow bold
-  echo "WARNING:$(colorize reset)" "$@"
+function log_warn() {
+  log "WARN" "$ORANGE" "$@"
 }
 
-function echo_done() {
-  colorize green bold
-  echo "DONE:$(colorize reset)" "$@"
+function log_success() {
+  log "OK" "$GREEN" "$@"
 }
 
-function echo_info() {
-  if [ "$#" -eq 1 ]; then
-    PREFIX="INFO"
-  else
-    PREFIX=$1
+function log_info() {
+  local LABEL="INFO"
+
+  if ! [ "$#" -eq 1 ]; then
+    LABEL=$(echo "$1" | tr [a-z] [A-Z])
     shift 1
   fi
 
-  colorize cyan bold
-  echo "${PREFIX}:$(colorize reset)" "$@"
+  log "${LABEL}" "$CYAN" "$@"
+}
+
+function help_section() {
+  local TITLE=$(echo "$@" | tr [a-z] [A-Z])
+  echo -e "${BOLD}${TITLE}${RESET}"
+}
+
+function load_env() {
+  local ENV_FILE=${1:-.env}
+  set -a
+  if [ -f "$ENV_FILE" ]; then
+    . "$ENV_FILE"
+  else
+    echo_error "Couldn't locate ${ENV_FILE} file..."
+  fi
+  set +a
 }
 
 function not_installed() {
   [ ! -x "$(command -v "$@")" ]
 }
 
-function load_env() {
-  ENV_FILE=${1:-.env}
-  set -a
-  if [ -f "$ENV_FILE" ]; then
-    source "$ENV_FILE"
-  else
-    echo_error "Couldn't locate ${ENV_FILE} file..."
-  fi
-  set +a
+function display_version() {
+    local program="$1"
+    local version="$2"
+    if not_installed figlet; then
+      echo "${program} script version ${version}"
+    else
+      echo -n "${BLUE}${BOLD}"
+      figlet "${program} script"
+      echo -n "${RESET}"
+      echo "version ${version}"
+    fi
 }
